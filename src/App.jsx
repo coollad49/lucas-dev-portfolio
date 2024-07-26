72ac10685e58848018772980ab226a70411eade9
Daniel <daniellucasadebayo@gmail.com>
2024-07-26 03:16:47 +0100

    add skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
 
 const App = ()=>{
   return(
     <div className="p-4 overflow-auto">
       <NavBar/>
       <Header/>
+      <Skills/>
     </div>
   )
 }
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,12 @@ import code from './code.png'
 import brand from './brand.png'
 import menu from './menu.png'
 import me from './me.jpeg'
+import djangoproject from './djangoproject.svg'
 
 export const assets = {
     code,
     brand,
     menu,
     me,
+    djangoproject
 }
