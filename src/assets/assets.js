8807de8ad2e54a12a60ca00667fb494029b68783
Lucas <coollad49@outlook.com>
2025-07-31 10:44:23 +0100

    change icon to lucide, add project

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,20 +1,10 @@
-import code from './code.png'
-import brand from './brand.png'
-import menu from './menu.png'
+import { Cloud, Box, ListTodo, Bot } from 'lucide-react'
 import me from './me.jpeg'
-import djangoproject from './djangoproject.svg'
-import cloudserver from './cloudserver.png'
-import box from './box.png'
-import task from './task.png'
+import code from './code.png'
 
 export const assets = {
-    code,
-    brand,
-    menu,
     me,
-    djangoproject,
-    cloudserver,
-    box,
+    code,
 }
 
 export const projects = [
@@ -29,7 +19,7 @@ export const projects = [
             `,
         skills: ['Python', 'REST', 'HTTP'],
         date: 'June 2024',
-        img: cloudserver,
+        icon: 'Cloud',
         two_buttons: false,
         link: 'https://github.com/coollad49/Http_server',
     },
@@ -44,7 +34,7 @@ export const projects = [
             `,
         skills: ['Python', 'Django', 'HTML', 'CSS', 'JavaScript'],
         date: 'May 2024',
-        img: box,
+        icon: 'Box',
         two_buttons: true,
         link: 'https://github.com/coollad49/PEAMS/',
         demo_link: 'https://peams.onrender.com',
@@ -60,10 +50,26 @@ export const projects = [
             `,
         skills: ['Python', 'HTML', 'Tailwind', 'Jquery', 'REST API', 'Javascript', 'CSS'],
         date: 'Jun - Jul 2024',
-        img: task,
+        icon: 'ListTodo',
         two_buttons: true,
         link: 'https://github.com/coollad49/task-management-dashboard-application',
         demo_link: 'https://task-management-dashboard-application.onrender.com/',
     },
-    
+    {
+        title: "Customer Support Chatbot",
+        description: "A customer support app for Living Well Hospital using Meta LLaMA, deployed via EC2. Built with Next.js, Tailwind CSS, and shadcn-ui, it provides intelligent, real-time support for hospital customers.",
+        task: `
+            - Developed a customer support chatbot using Meta LLaMA <br />
+            - Integrated with hospital systems for real-time support <br />
+            - Deployed on AWS EC2 for scalability <br />
+            - Built with Next.js, Tailwind CSS, and shadcn-ui <br />
+            - Provides intelligent, context-aware responses to user queries
+            `,
+        skills: ['Next.js', 'JavaScript', 'Meta LLaMA', 'Tailwind CSS', 'shadcn-ui', 'AWS EC2'],
+        date: '2024',
+        icon: 'Bot',
+        two_buttons: true,
+        link: 'https://github.com/coollad49/customer_support_chatbot',
+        demo_link: 'http://35.183.131.3/',
+    },
 ]
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import * as LucideIcons from 'lucide-react';
 
-const Project = ({img, title, description, task, skills, link, date, two_buttons, demo_link}) => {
+const Project = ({icon, title, description, task, skills, link, date, two_buttons, demo_link}) => {
+  const IconComponent = LucideIcons[icon] || LucideIcons['Box'];
   return (
     <div className="dark:bg-gray-900 bg-gray-100 dark:border-none border shadow-xl p-1 md:p-5 rounded-xl flex flex-col mb-5 md:flex-row-reverse md:justify-between">
         <div className="flex justify-center my-5 md:items-center">
-            <img src={img} alt="" className="w-40 md:w-80  hover:scale-105 transition"/>
+            <IconComponent className="w-24 h-24 md:w-40 md:h-40 hover:scale-105 transition" />
         </div>
         <div className=''>
             <div className="flex flex-col items-center md:items-start md:p-2 dark:text-white text-gray-900">
@@ -12,10 +14,10 @@ const Project = ({img, title, description, task, skills, link, date, two_buttons
                 <h3 className="my-2">{date}</h3>
             </div>
             <p className="p-2 dark:text-gray-200 text-gray-800 font-medium md:max-w-md">{description}</p>
-            <p className="p-2 dark:text-gray-200 text-gray-800 font-medium">
+            <div className="p-2 dark:text-gray-200 text-gray-800 font-medium" style={{marginTop: 0}}>
                 <b>During the course of this project,</b><br />
                 <div dangerouslySetInnerHTML={{ __html: task }} />
-            </p>
+            </div>
             <div className="p-2 flex flex-wrap gap-2">
                 {skills.map((skill, index)=>(
                     <div key={index} className="bg-gray-700 px-2 rounded font-semibold">{skill}</div>
@@ -25,21 +27,19 @@ const Project = ({img, title, description, task, skills, link, date, two_buttons
                 {two_buttons ? <a href={demo_link} target='_blank' className="flex justify-center my-3 cursor-pointer"><span className="dark:bg-white bg-gray-900 dark:border-2 font-bold rounded-full py-3 px-4 text-white dark:text-blue-900">View Demo</span></a> : ''}
                 <a href={link} className="flex justify-center my-3 cursor-pointer"><span className="dark:bg-gray-800 bg-gray-900 font-bold rounded-full py-3 px-4">View Project Details</span></a>
             </div>
-            
         </div>
     </div>
   )
 }
 
-
 Project.propTypes = {
-    img: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     task: PropTypes.string.isRequired,
     skills: PropTypes.arrayOf(PropTypes.string).isRequired,
     link: PropTypes.string.isRequired,
-    demo_link: PropTypes.string.isRequired,
+    demo_link: PropTypes.string,
     date: PropTypes.string.isRequired,
     two_buttons: PropTypes.bool.isRequired,
 };
