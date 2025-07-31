e5be93ac78b4947288ffab11b580aa733bb3e4ba
Lucas <coollad49@outlook.com>
2025-07-31 12:56:11 +0100

    update about

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="dark:text-white flex flex-col items-center pb-6 max-w-md md:items-start">
             <h2 className="font-bold text-xl md:text-2xl mb-2">Hi, my name is</h2>
             <h1 className="font-bold text-3xl md:text-4xl mb-3">Lucas-Adebayo Daniel</h1>
-            <p className="text-sm text-center dark:text-gray-300 text-gray-900 md:text-left">I am a Software Engineering Intern with hands-on experience in full-stack development, specializing in Django and Tailwind CSS. Proven ability to deliver solutions in agile & CI/CD environments.</p>
+            <p className="text-sm text-center dark:text-gray-300 text-gray-900 md:text-left">I am a Software Engineer with hands-on experience in full-stack development, specializing in Django and Nextjs. With proven ability to deliver solutions in agile & CI/CD environments.</p>
             <div className="w-full dark:bg-white dark:text-black bg-black/90 text-white flex justify-around mt-7 p-4 rounded-xl">
                 <div className="flex flex-col items-center">
                     <span className="font-bold text-3xl">12</span>
