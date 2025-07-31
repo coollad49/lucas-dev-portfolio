9d3ba938eefd208cc9725f09448c0af770b87bb2
Lucas <coollad49@outlook.com>
2025-07-31 13:10:56 +0100

    update logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="dark:text-white flex space-x-3 items-center py-5">
         <img src={assets.code} alt="" className="w-10 rounded-md"/>
-        <h2 className="text-3xl font-medium  hidden md:block tracking-wide ">lucas dev</h2>
+        <h2 className="text-3xl font-medium  hidden md:block tracking-wide ">LucasBuilds</h2>
     </div>
     
   )
